b0e62552637e42f37ff238cc6332472ff34a2a5f
Bosspug <eugeniojlopezt@gmail.com>
2024-02-13 17:01:56 -0500

    player moves and has better mouse sens

--- Quantum Stones/Assets/Scripts/PlayerMovement.cs
+++ Quantum Stones/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public CharacterController controller;
+
+    [SerializeField] public float speed = 12f;
+    [SerializeField] public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
