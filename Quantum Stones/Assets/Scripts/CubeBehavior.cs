dcbff878861440b0d191e5907009afa263f7722c
Bosspug <eugeniojlopezt@gmail.com>
2024-02-14 21:57:54 -0500

    Cube Behavior Finished, Environment started

---
+++ Quantum Stones/Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+
+    [SerializeField] private Collider scanner;
+    [SerializeField] private Rigidbody cube;
+    [SerializeField] private Transform cubeTrans;
+    [SerializeField] private Transform center;
+    [SerializeField] private Collider player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == player)
+        {
+            transform.RotateAround(center.position, Vector3.up, 20f);
+            new Vector3(0f, 0f, 0f);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other == player)
+        {
+            new Vector3(0f, 0f, 0f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == scanner)
+        {
+            transform.RotateAround(center.position, Vector3.up, Random.Range(5f, 25f)); 
+        }
+    }
+  
+}
--- Quantum Stones/Assets/Scripts/QuantumBehavior.cs
+++ Quantum Stones/Assets/Scripts/QuantumBehavior.cs
@@ -29,19 +29,5 @@ public class QuantumBehavior : MonoBehaviour
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-
-        
-
-    }
+  
 }
